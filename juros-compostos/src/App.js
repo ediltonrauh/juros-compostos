68e865ca55c9cfba820117620bbce53a2dd31b07
ediltonrauh <70674998+ediltonrauh@users.noreply.github.com>
2020-09-03 00:46:50 -0300

    Add files via upload

---
+++ juros-compostos/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Valores from './components/valores/valores';
+
+export default () => {
+  useEffect(() => {
+    effect;
+    return () => {
+      cleanup;
+    };
+  }, [input]);
+  return <Valores />;
+};
